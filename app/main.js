fde0cad6720b2825a4f6f85033fc4a06c52dfac9
Rey Ramos <reymundo.ramos@gmail.com>
2015-09-02 12:46:19 -0400

    updates

--- app/main.js
+++ app/main.js
@@ -12,9 +12,13 @@ var angular = require('./lib/ng-electron/ng-bridge');
 
 function createMainWindow() {
     const win = new BrowserWindow({
-        width: 1500,
-        height: 600,
-        frame: false
+        'min-width': 1250,
+        'width': 1250,
+        'min-height': 800,
+        'height': 800,
+        'resizable': true,
+        //'fullscreen': true,
+        //'frame': false,
     });
 
     win.loadUrl('file://' + __dirname + '/index.html');
@@ -43,21 +47,24 @@ app.on('activate-with-no-open-windows', function () {
 
 app.on('will-quit', function () {
     console.log('<====================================>');
-    console.log('Amy Says, "Stay Awesome Kids!"');
+    console.log('Electron Says, "Stay Awesome Kids!"');
     console.log('<====================================>');
 });
 
 app.on('ready', function () {
     mainWindow = createMainWindow();
     console.log('<====================================>');
-    console.log("Amy says, \"Let's Code Awesome!\"");
+    console.log("Electron says, \"Let's Code Awesome!\"");
     console.log('<====================================>');
     mainWindow.webContents.on('dom-ready', function (e) {
         //try and manually bootstrap AngularJS
-        //The application will be already bootstrap
         //var code = "angular.bootstrap(document, ['app']);"
         //mainWindow.webContents.executeJavaScript( code );
+        console.log('<====================================>');
+        console.log("Electron says, \"Application is already Bootstrapped!\"");
+        console.log('<====================================>');
 
+        angular.send( "Hello from Electron" );
     });
 
     mainWindow.openDevTools();

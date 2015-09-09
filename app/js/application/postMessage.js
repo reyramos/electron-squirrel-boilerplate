50b84064fa0f240a2e2dae073e9ef7e6c6ce2245
Rey Ramos <reymundo.ramos@gmail.com>
2015-09-09 08:42:54 -0400

    update

--- app/js/application/postMessage.js
+++ app/js/application/postMessage.js
@@ -1,8 +1,23 @@
 /**
- * # Application
+ * @ngdoc service
+ * @name postMessage
  *
- * Core Application controller that includes functions used before we kickStart the Application
- * The functions store within this files live outside of the ngView and are used as global function
+ * @description
+ * # Window.postMessage() management service
+ *
+ * The window.postMessage method allows for cross-origin communication. This service will provide a message management using
+ * promises.
+ *
+ * The parent will start listening for any event data.
+ * postMessage.send(eventType[optional],data).then(function(data){}),
+ * It will attach a $q service,
+ * https://docs.angularjs.org/api/ng/service/$q
+ *
+ * postMessage.send(eventType[optional],data).then(function(data){})
+ * EventType are optional, it will add a promise_id for $q functionality
+ * Since postMessage can be asynchronous, return messages can return out of order.  The promise_id, will determine
+ * the send messages and promise for each.
+ *
  */
 
 (function (angular) {
@@ -10,81 +25,117 @@
 
     angular.module('app').service('postMessage', PostMessage);
 
-    PostMessage.$inject = ['$window', '$q'];
+    PostMessage.$inject = ['$window', '$q']
 
     function PostMessage($window, $q) {
         var self = this,
-            parent = $window.parent || window.parent;
+            parent = $window.parent || window.parent,
+            currentCallbackId = 0, // Create a unique callback ID to map requests to responses
+            service = {
+                callbacks: [], // Keep all pending requests here until they get responses
+                onmessage: []
+            };
+
+        // This creates a new callback ID for a request
+        function getCallbackId() {
+            currentCallbackId += 1;
+            //reset callback id
+            if (currentCallbackId > 10000) {
+                currentCallbackId = 0;
+            }
+            return currentCallbackId;
+        }
+        /**
+         * Will check if promise has been sent to return back to
+         * defer.promise() message
+         */
+        function listening() {
+            if (service.onmessage.hasOwnProperty(this.promise)) {
+                service.onmessage[this.promise].cb.resolve(this)
+                delete service.onmessage[this.promise];
+                delete this.promise;
+            }
+        }
+
+        function onMessage(data) {
+            var msg = data.message || data;
+            listening.apply(msg)
+        }
 
         $window.addEventListener('message', function (e) {
 
             var data = e.data;
-
-            if (typeof (data) === "undefined") return;
-
+            if (typeof (data) === "undefined" || e.source == this) return;
             try {
                 data = JSON.parse(data);
             } catch (e) {
             }
 
-
+            /**
+             * When a message is return, from sender, we would
+             * need to change to response
+             */
             switch (data.cmd) {
-                case 'send':
+                case 'request':
                     var eventType = data.eventType,
-                    	msg = data.message;
+                        msg = angular.copy(data.message);
 
                     if (eventType) {
-                    	//DO SOMETHING FOR EVENT TYPES FROM IFRAME
+                        //DO SOMETHING FOR EVENT TYPES FROM IFRAME
+                        //This is to add custom code for types of events requested from the
+                        //sender
                     } else {
-
                     }
-                    //port.postMessage(JSON.stringify({type: '_send', data: msg}));
+                    e.source.postMessage(JSON.stringify({cmd: 'response', message: msg}), e.origin);
+                    break;
+                case 'response':
+                    onMessage(data);
                     break;
                 default:
-                    //port.postMessage(JSON.stringify(data));
                     break;
             }
 
-
-            console.log('From Child:  ', e);
         });
 
 
-        self.send = function (element, data, callback) {
+        self.send = function (eventType, data, callback) {
 
-            var etype = typeof arguments[0],
+            var defer = $q.defer(),
+                callback_id = getCallbackId(),
+                etype = typeof arguments[0],
                 dtype = typeof arguments[1];
 
             data = data || {};
 
 
-            if (etype[0]) {
-                //iframe = etype[0].contentWindow
+            if (etype === 'object') {
+                data = eventType;
+                eventType = (typeof(data.promise) === "undefined" ? callback_id : data.promise);
             }
 
-            //if (dtype === 'function') {
-            //    callback = data || null;
-            //}
-            //
-            //data.promise = callback_id;
-            //
-            ////set the caller
-            //service.onmessage[callback_id] = {
-            //    time: new Date(),
-            //    cb: defer,
-            //    callback: callback
-            //};
-            //
-            //var postMessage = {
-            //    cmd: 'send',
-            //    message: angular.copy(data),
-            //    eventType: typeof(arguments[0]) === 'object' ? false : eventType,
-            //    callback: callback ? callback.toString() : null
-            //}
-            //
-            //console.log('postMessage', postMessage)
-            ////TODO://narrow this to a local domain for security
-            //parent.postMessage(JSON.stringify(postMessage), '*'); //allow all domains
+            if (dtype === 'function') {
+                callback = data || null;
+            }
+
+            data.promise = callback_id;
+
+            //set the caller
+            service.onmessage[callback_id] = {
+                time: new Date(),
+                cb: defer,
+                callback: callback
+            };
+
+            var postMessage = {
+                cmd: 'request',
+                message: angular.copy(data),
+                eventType: typeof(arguments[0]) === 'object' ? false : eventType,
+                callback: callback ? callback.toString() : null
+            }
+
+            console.log('postMessage', postMessage)
+            parent.postMessage(JSON.stringify(postMessage), '*'); //allow all domains
+            return defer.promise;
         }
     }
 

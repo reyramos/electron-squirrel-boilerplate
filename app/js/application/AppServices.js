38d50bf01f45ed6f9d5ec6a22a8bcff487912854
Rey Ramos <reymundo.ramos@gmail.com>
2015-09-14 15:04:14 -0400

    updates

--- app/js/application/AppServices.js
+++ app/js/application/AppServices.js
@@ -1,9 +1,3 @@
-/**
- * # Application
- *
- * Core Application controller that includes functions used before we kickStart the Application
- * The functions store within this files live outside of the ngView and are used as global function
- */
 
 (function (angular) {
     'use strict';

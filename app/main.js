5c70c780c9ca79fab91fd8ccf6f49ef94e32a9c7
Rey Ramos <reymundo.ramos@gmail.com>
2015-09-20 14:04:37 -0400

    updates

--- app/main.js
+++ app/main.js
@@ -1,14 +1,43 @@
-/**
- * Amy is Awesome!
- */
 'use strict';
 var path = require('path');
 var app = require('app');
 var ipc = require('ipc');
-
+var http = require("http");
+var https = require("https");
 var BrowserWindow = require('browser-window');
-
 var angular = require('./lib/ng-electron/ng-bridge');
+
+/**
+ * getJSON:  REST get request returning JSON object(s)
+ * @param options: http options object
+ * @param callback: callback to pass the results JSON object(s) back
+ */
+function getJSON(options, onResult) {
+    console.log("rest::getJSON");
+
+    var prot = options.port == 443 ? https : http;
+    var req = prot.request(options, function (res) {
+        var output = '';
+        console.log(options.host + ':' + res.statusCode);
+        res.setEncoding('utf8');
+
+        res.on('data', function (chunk) {
+            output += chunk;
+        });
+
+        res.on('end', function () {
+            var obj = JSON.parse(output);
+            onResult(res.statusCode, obj);
+        });
+    });
+
+    req.on('error', function (err) {
+        onResult(err.message);
+    });
+
+    req.end();
+};
+
 
 function createMainWindow() {
     const win = new BrowserWindow({
@@ -60,7 +89,25 @@ app.on('ready', function () {
         angular.send("Hello from Electron");
     });
 
-    //mainWindow.openDevTools();
+
+    var options = {
+        host: 'http://24.211.139.167/build.json',
+        //port: 443,
+        //path: '/some/path',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    getJSON(options,
+        function (statusCode, result) {
+            angular.send(statusCode);
+            angular.send(JSON.stringify(result));
+        });
+
+
+    mainWindow.openDevTools();
     //mainWindow.print();
 
     mainWindow.webContents.on('did-finish-load', function (e) {

cd616e558138d3a8fd8471d9b58befef810f7e06
Rey Ramos <reymundo.ramos@gmail.com>
2015-10-08 10:17:24 -0400

    major upgrade code changes

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 'use strict';
 const BrowserWindow = require('browser-window');
 const Menu = require('menu');
-const angular = require('./lib/ng-electron/ng-bridge');
+const angular = require('./ng-electron/ng-bridge');
 const http = require("http");
 const path = require('path');
 const ipc = require('ipc');
@@ -10,9 +10,12 @@ const fs = require('fs');
 const dialog = require('dialog');
 const version = require('./version.json');
 const MenuItem = require('menu-item');
+const utilities = require('./utilities.js');
 
 const urlBuilds = "http://dev-eligibility-phoenix.labcorp.com/reyramos/builds/";
 
+// prevent window being GC'd
+let mainWindow;
 
 /**
  * getJSON:  REST get request returning JSON object(s)
@@ -20,7 +23,7 @@ const urlBuilds = "http://dev-eligibility-phoenix.labcorp.com/reyramos/builds/";
  * @param callback: callback to pass the results JSON object(s) back
  */
 function getVersion(callback) {
-    http.get(urlBuilds + "test.json", function (res) {
+    http.get(urlBuilds + "build.json", function (res) {
         var output = '';
         res.setEncoding('utf8');
 
@@ -36,56 +39,6 @@ function getVersion(callback) {
     }).on('error', function (e) {
         callback(e);
     });
-
-
-}
-
-
-function versionCompare(v1, v2, options) {
-    var lexicographical = options && options.lexicographical,
-        zeroExtend = options && options.zeroExtend,
-        v1parts = v1.split('.'),
-        v2parts = v2.split('.');
-
-    function isValidPart(x) {
-        return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
-    }
-
-    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
-        return NaN;
-    }
-
-    if (zeroExtend) {
-        while (v1parts.length < v2parts.length) v1parts.push("0");
-        while (v2parts.length < v1parts.length) v2parts.push("0");
-    }
-
-    if (!lexicographical) {
-        v1parts = v1parts.map(Number);
-        v2parts = v2parts.map(Number);
-    }
-
-    for (var i = 0; i < v1parts.length; ++i) {
-        if (v2parts.length == i) {
-            return 1;
-        }
-
-        if (v1parts[i] == v2parts[i]) {
-            continue;
-        }
-        else if (v1parts[i] > v2parts[i]) {
-            return 1;
-        }
-        else {
-            return -1;
-        }
-    }
-
-    if (v1parts.length != v2parts.length) {
-        return -1;
-    }
-
-    return 0;
 }
 
 
@@ -98,52 +51,132 @@ function createMainWindow() {
         title: 'LabCorp Phoenix'
     });
 
-    win.loadUrl('file://' + __dirname + '/index.html');
-    win.on('closed', onClosed);
+    win.loadUrl('https://dev-demographics-phoenix.labcorp.com/web-ui');
+    win.on('closed', function(){
+        mainWindow = null;
+    });
 
     return win;
 }
 
-function onClosed() {
-    mainWindow = null;
-}
-// prevent window being GC'd
-let mainWindow;
 
+/**
+ * Create the main Electron Application
+ */
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit();
     }
-});
-
-app.on('activate-with-no-open-windows', function () {
+}).on('activate-with-no-open-windows', function () {
     if (!mainWindow) {
         mainWindow = createMainWindow();
     }
-});
-
-app.on('will-quit', function () {
+}).on('will-quit', function () {
     console.log('<====================================>');
     console.log('Goodbye');
-});
-
-app.on('ready', function () {
+}).on('ready', function () {
     mainWindow = createMainWindow();
     mainWindow.webContents.on('dom-ready', function (e) {
-        //try and manually bootstrap AngularJS
-        //mainWindow.webContents.executeJavaScript(bootstrap);
+        mainWindow.webContents.executeJavaScript("document.documentElement.setAttribute('id','ELECTRON_PARENT_CONTAINER');");
     });
 
-    //mainWindow.openDevTools()
+    //open the developer tools
+    mainWindow.openDevTools();
+
+    var template = [
+        {
+            label: 'Edit',
+            submenu: [
+                {
+                    label: 'Undo',
+                    accelerator: 'CmdOrCtrl+Z',
+                    role: 'undo'
+                },
+                {
+                    label: 'Redo',
+                    accelerator: 'Shift+CmdOrCtrl+Z',
+                    role: 'redo'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Cut',
+                    accelerator: 'CmdOrCtrl+X',
+                    role: 'cut'
+                },
+                {
+                    label: 'Copy',
+                    accelerator: 'CmdOrCtrl+C',
+                    role: 'copy'
+                },
+                {
+                    label: 'Paste',
+                    accelerator: 'CmdOrCtrl+V',
+                    role: 'paste'
+                },
+                {
+                    label: 'Select All',
+                    accelerator: 'CmdOrCtrl+A',
+                    role: 'selectall'
+                },
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                {
+                    label: 'Reload',
+                    accelerator: 'CmdOrCtrl+R',
+                    click: function(item, focusedWindow) {
+                        if (focusedWindow)
+                            focusedWindow.reload();
+                    }
+                },
+                {
+                    label: 'Toggle Full Screen',
+                    accelerator: (function() {
+                        if (process.platform == 'darwin')
+                            return 'Ctrl+Command+F';
+                        else
+                            return 'F11';
+                    })(),
+                    click: function(item, focusedWindow) {
+                        if (focusedWindow)
+                            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+                    }
+                },
+                //{
+                //    label: 'Toggle Developer Tools',
+                //    accelerator: (function() {
+                //        if (process.platform == 'darwin')
+                //            return 'Alt+Command+I';
+                //        else
+                //            return 'Ctrl+Shift+I';
+                //    })(),
+                //    click: function(item, focusedWindow) {
+                //        if (focusedWindow)
+                //            focusedWindow.toggleDevTools();
+                //    }
+                //},
+            ]
+        }
+    ];
+
+    var menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+
+
     mainWindow.webContents.on('did-finish-load', function (e) {
 
         setTimeout(function () {
             angular.send('hello from electron');
             getVersion(function (status, obj) {
                 console.log('<====================================>');
-                console.log('obj', obj);
+                console.log('version => ', obj);
 
-                var vrsCompare = versionCompare(obj.version, version.version);
+                var vrsCompare = utilities.versionCompare(obj.version, version.version);
                 if (vrsCompare > 0) {
                     mainWindow.close();
                     var download = new BrowserWindow({
@@ -155,7 +188,7 @@ app.on('ready', function () {
                         'always-on-top': true
                     });
 
-                    download.loadUrl('file://' + __dirname + '/dialogs/download.html?version='+obj.version);
+                    download.loadUrl('file://' + __dirname + '/dialogs/download.html?version=' + obj.version);
                     download.on('closed', function () {
                         download = null;
                     })
@@ -163,17 +196,18 @@ app.on('ready', function () {
                     //lets close it after 5 minutes
                     setTimeout(function () {
                         download.close();
-                    },1000 * 60 * 5)
+                    }, 1000 * 60 * 5)
 
                 }
             });
 
-        }, 500)
-
+        }, 500);
 
         //Start listening for client messages
         angular.listen(function (msg) {
             console.log('Client: ' + msg);
         });
+
     });
+
 });

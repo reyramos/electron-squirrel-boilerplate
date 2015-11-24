492a5c91dfe3d168727b467c2bb470007d7ef5d8
Rey Ramos <reymundo.ramos@gmail.com>
2015-11-24 15:24:07 -0500

    updates and fix

--- electron.config.js
+++ electron.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "version": "1.2.11",
+    "version": "1.2.12",
     "app_name": "LabCorp Phoenix",
     "app_description": "LabCorp Application",
     "manufacturer": "LabCorp Inc",
@@ -8,11 +8,12 @@ module.exports = {
     "distribution": "build",
     "versionFilePath": "/release/[WORKING_ENVIRONMENT]/build.json",
     "electron_build": "./electron-v0.35.0-win32-x64",
-    "UAT": "https://uat-phoenix.labcorp.com/web-ui/#/",
-    "DEV": "https://dev-demographics-phoenix.labcorp.com/web-ui/#/",
-    "QA": "https://qa-phoenix.labcorp.com/web-ui/#/",
-    "BETA": "https://qa-phoenix.labcorp.com/web-ui/#/",
-    "VERSION_SERVER": "https://dev-phoenix.labcorp.com/web-ui/#/",
-    "WORKING_ENVIRONMENT": "DEV"
+    "UAT": "https://uat-phoenix.labcorp.com/web-ui/",
+    "PROD": "https://phoenix.labcorp.com/web-ui/",
+    "DEV": "https://dev-demographics-phoenix.labcorp.com/web-ui/",
+    "QA": "https://qa-phoenix.labcorp.com/web-ui/",
+    "BETA": "https://qa-phoenix.labcorp.com/web-ui/",
+    "VERSION_SERVER": "https://dev-phoenix.labcorp.com/web-ui/",
+    "WORKING_ENVIRONMENT": "PROD"
 };
 

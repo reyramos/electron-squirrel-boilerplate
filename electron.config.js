ee81dc4a43bd2e04bd0cbd1904d66dfed1685383
Rey Ramos <reymundo.ramos@gmail.com>
2015-12-07 20:49:08 -0500

    updates

--- electron.config.js
+++ electron.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "version": "1.3.2",
+    "version": "1.3.3",
     "app_name": "LabCorp Phoenix",
     "app_description": "LabCorp Application",
     "manufacturer": "LabCorp Inc",

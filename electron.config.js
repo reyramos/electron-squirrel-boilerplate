77ab4f236b559ec0dfd8b5eeac235d97b74c73a3
Rey Ramos <ramor11@labcorp.com>
2016-08-11 09:43:51 -0400

    update to CHANGELOG and build release version 1.5.3

--- electron.config.js
+++ electron.config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     //for every build on msi the numbers will need to increment to update the
     //msi application stored on windows machine
-    "version": "1.5.1",
+    "version": "1.5.3",
 
     "app_name": "LabCorp Phoenix",
     "app_description": "LabCorp Application",
@@ -33,7 +33,6 @@ module.exports = {
     "QA": "https://qa-phoenix.labcorp.com/web-ui/",
     "BETA": "https://dev-phoenix.labcorp.com/web-ui/",
     "RELEASE2": "http://dev2-phoenix.labcorp.com/web-ui/?debug=true",
-    // "LOCAL": "file://__dirname/demo/index.html",
     "LOCAL": "http://seed.labcorp.com:8080/",
 
     //hosting server setting and working environment
@@ -42,7 +41,7 @@ module.exports = {
     //environment where the build.json is going to be stored for the working environment
     "versionFilePath": "/release/[WORKING_ENVIRONMENT]/build.json",
     //environment to build on start up
-    "WORKING_ENVIRONMENT": "LOCAL"
+    "WORKING_ENVIRONMENT": "RELEASE2"
 
 };
 
